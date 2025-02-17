81b2fa56d5ac4209b9c3a11745711ce1a6d4fe9c
yayyaysu <ian1231231236@gmail.com>
2025-02-17 22:20:24 +0800

    get api修正

--- my-website/src/api/api.js
+++ my-website/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 設定 Flask 伺服器的 URL
-const API_BASE_URL = 'http://127.0.0.1:5000/api/data ';  // Flask 後端 API 位置
+const API_BASE_URL = 'http://127.0.0.1:5000/api';  // Flask 後端 API 位置
 
 const api = axios.create({
   baseURL: API_BASE_URL,
